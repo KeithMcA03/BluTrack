d289bf6d36abae5c46ba4caf96bfe64691cd6e29
KeithMcA03 <keith.mca4@icloud.com>
2025-04-14 15:57:06 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,1 @@
-import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'blutrack.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = 'your-secret-key'
