11741fde8b9784edc6cfe6d2f67fac76b796b60e
Keith McAllister <your-email@example.comkeith.mca4@icloud.com>
2025-04-17 13:44:15 -0400

    commit

--- db_connect.py
+++ db_connect.py
@@ -1,18 +1,9 @@
 import mysql.connector
 
-# Connecting to the database
-conn = mysql.connector.connect(
-    host="10.209.55.95",          
-    user="bis425_gr4",      
-    password="sWjgL38VD$"     
-)
-
-cursor = conn.cursor()
-cursor.execute("SHOW TABLES;")
-
-# Print out the tables
-for table in cursor.fetchall():
-    print(table)
-
-cursor.close()
-conn.close()
+def get_connection():
+    return mysql.connector.connect(
+        host="10.209.55.95",
+        user="bis425_g4",
+        password="sWjgl38VD$",
+        database="bis425_g4"
+    )
